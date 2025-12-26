43421d886908ec03cfaae606e1c7d7eee87be9c6
yuseonghun <74244668+yuseonghun@users.noreply.github.com>
2025-12-26 17:39:09 +0900

    test: ci flow check (#1)
    test: verify CI flow

--- studylog/src/main/java/com/studylog/api/StudylogApiApplication.java
+++ studylog/src/main/java/com/studylog/api/StudylogApiApplication.java
@@ -2,12 +2,20 @@ package com.studylog.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class StudylogApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudylogApiApplication.class, args);
+	}
+	@GetMapping("/study-check")
+	public String studyCheck() {
+		return "STUDYLOG SERVER OK";
+		// CI flow test comment
 	}
 
 }
