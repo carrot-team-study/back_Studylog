7c9c2bccfc7a7de5a14a231913a25ee2148c7930
kimchaeryn <ryn3439@naver.com>
2025-12-29 13:55:31 +0900

    modify

--- studylog/src/main/java/com/studylog/api/StudylogApiApplication.java
+++ studylog/src/main/java/com/studylog/api/StudylogApiApplication.java
@@ -2,12 +2,13 @@ package com.studylog.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class StudylogApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StudylogApiApplication.class, args);
+        SpringApplication.run(StudylogApiApplication.class, args);
 	}
 
 }
