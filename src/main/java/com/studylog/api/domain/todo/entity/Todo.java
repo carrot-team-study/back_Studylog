dfb948adbe3b194c09334e4f897e0ed11a0a0978
kimchaeryn <ryn3439@naver.com>
2026-01-14 14:02:37 +0900

    add JPA dependency to build.gradle

--- src/main/java/com/studylog/api/domain/todo/entity/Todo.java
+++ src/main/java/com/studylog/api/domain/todo/entity/Todo.java
@@ -1,4 +1,15 @@
 package com.studylog.api.domain.todo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity // DB 테이블과 연결
+@Table(name = "todo") // DB 테이블명 명시
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Todo {
 }
